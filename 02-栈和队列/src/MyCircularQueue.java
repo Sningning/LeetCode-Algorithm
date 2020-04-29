540840ab3e5ee64492a4ec874f5aab64a84bc411
Song Ningning <qfsnn1117@gmail.com>
2020-04-29 14:20:22 +0800

    622 solved

---
+++ 02-栈和队列/src/MyCircularQueue.java
@@ -0,0 +1,103 @@
+/**
+ * 622. 设计循环队列【中等】
+ * 设计你的循环队列实现。 循环队列是一种线性数据结构，其操作表现基于 FIFO（先进先出）原则并且队尾被连接在队首之后以形成一个循环。
+ * 它也被称为“环形缓冲器”。
+ *
+ * 循环队列的一个好处是我们可以利用这个队列之前用过的空间。
+ * 在一个普通队列里，一旦一个队列满了，我们就不能插入下一个元素，即使在队列前面仍有空间。但是使用循环队列，我们能使用这些空间去存储新的值。
+ *
+ * 你的实现应该支持如下操作：
+ *
+ * MyCircularQueue(k): 构造器，设置队列长度为 k 。
+ * Front: 从队首获取元素。如果队列为空，返回 -1 。
+ * Rear: 获取队尾元素。如果队列为空，返回 -1 。
+ * enQueue(value): 向循环队列插入一个元素。如果成功插入则返回真。
+ * deQueue(): 从循环队列中删除一个元素。如果成功删除则返回真。
+ * isEmpty(): 检查循环队列是否为空。
+ * isFull(): 检查循环队列是否已满。
+ *
+ *
+ * @Author: Song Ningning
+ * @Date: 2020-04-29 11:46
+ */
+public class MyCircularQueue {
+
+    private int[] arr;
+    private int size;
+    private int front;
+    private int rear;
+
+    /** Initialize your data structure here. Set the size of the queue to be k. */
+    public MyCircularQueue(int k) {
+        if (k < 0) {
+            throw new IllegalArgumentException("The init size is less than 0");
+        }
+        this.arr = new int[k];
+        this.size = 0;
+        this.front = 0;
+        this.rear = 0;
+    }
+
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    public boolean enQueue(int value) {
+        if (size == arr.length) {
+            return false;
+        }
+        arr[rear] = value;
+        size ++;
+        rear = rear == arr.length - 1 ? 0 : rear + 1;
+        return true;
+    }
+
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    public boolean deQueue() {
+        if (size == 0) {
+            return false;
+        }
+        front = front == arr.length - 1 ? 0 : front + 1;
+        size--;
+        return true;
+    }
+
+    /** Get the front item from the queue. */
+    public int Front() {
+        if (size == 0) {
+            return -1;
+        }
+        return arr[front];
+    }
+
+    /** Get the last item from the queue. */
+    public int Rear() {
+        if (size == 0) {
+            return -1;
+        }
+        return rear == 0 ? arr[arr.length - 1] : arr[rear - 1];
+    }
+
+    /** Checks whether the circular queue is empty or not. */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /** Checks whether the circular queue is full or not. */
+    public boolean isFull() {
+        return size == arr.length;
+    }
+
+
+    public static void main(String[] args) {
+        MyCircularQueue queue = new MyCircularQueue(3);
+        System.out.println(queue.enQueue(1));
+        System.out.println(queue.enQueue(2));
+        System.out.println(queue.enQueue(3));
+        System.out.println(queue.enQueue(4));
+        System.out.println(queue.Rear());
+        System.out.println(queue.isFull());
+        System.out.println(queue.deQueue());
+        System.out.println(queue.enQueue(4));
+        System.out.println(queue.Rear());
+
+    }
+
+}
