f33581fb44f346176092a1602fda875be97d09af
Song Ningning <qfsnn1117@gmail.com>
2020-04-30 10:57:01 +0800

    update code

--- 01-数组和链表/src/array/TwoSum.java
+++ 01-数组和链表/src/array/TwoSum.java
@@ -40,10 +40,31 @@ public class TwoSum {
         // return new int[0];
 
 
-        // 2. 哈希表。
-        // 由于哈希查找的时间复杂度为 O(1)，所以可以利用哈希容器 map 降低时间复杂度
-        // 遍历数组 nums，i 为当前下标，每个值都判断map中是否存在 target-nums[i] 的 key 值
+
+        // 2. 两遍哈希表。
+
+        // 先将数组中每个数值和索引存入哈希表，然后再遍历数组，查看target - nums[i] 是否已经存在于哈希表中。
+        // 第一遍时间复杂度是 O(N)，第二遍从哈希表中查找的时间复杂度是O(1)，整体时间复杂度：O(N)
+        // 空间复杂度O(N)
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for (int i = 0; i < nums.length; i++) {
+        //     map.put(nums[i], i);
+        // }
+        // for (int i = 0; i < nums.length; i++) {
+        //     int key = target - nums[i];
+        //     if (map.containsKey(key) && map.get(key) != i)
+        //         return new int[]{i, map.get(key)};
+        // }
+        // return new int[0];
+
+
+
+        // 3. 一遍哈希表。
+
+        // 存入哈希表的同时去检查 map 中是否存在 target-nums[i] 的 key 值
         // 如果存在则找到了两个值，如果不存在则将当前的 (nums[i],i) 存入 map 中，继续遍历直到找到为止
+        // 时间复杂度：O(N)，空间复杂度O(N)
 
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
@@ -53,6 +74,7 @@ public class TwoSum {
             map.put(nums[i], i);
         }
         return new int[0];
+
 
     }
 
