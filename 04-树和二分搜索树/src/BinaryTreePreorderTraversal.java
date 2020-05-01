a371259082b5e48e532b71072c0f89e133b9d730
Song Ningning <qfsnn1117@gmail.com>
2020-05-01 10:40:51 +0800

    144 solved

---
+++ 04-树和二分搜索树/src/BinaryTreePreorderTraversal.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+/**
+ * 144. 二叉树的前序遍历
+ * 给定一个二叉树，返回它的 前序 遍历。
+ *
+ * https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+ *
+ * @Author: Song Ningning
+ * @Date: 2020-05-01 10:03
+ */
+public class BinaryTreePreorderTraversal {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        // 1. 递归
+
+        List<Integer> res = new ArrayList<>();
+        preOrder(root, res);
+        return res;
+
+
+        // 2. 迭代
+
+        // List<Integer> res = new ArrayList<>();
+        // if (root == null) return res;
+        // Deque<TreeNode> stack = new ArrayDeque<>();
+        // stack.push(root);
+        // while (!stack.isEmpty()) {
+        //     TreeNode node = stack.pop();
+        //     res.add(node.val);
+        //     if (node.right != null)
+        //         stack.push(node.right);
+        //     if (node.left != null)
+        //         stack.push(node.left);
+        // }
+        // return res;
+
+    }
+
+    private void preOrder(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        list.add(node.val);
+        preOrder(node.left, list);
+        preOrder(node.right, list);
+    }
+
+}
---
+++ 04-树和二分搜索树/src/TreeNode.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: Song Ningning
+ * @Date: 2020-05-01 10:03
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
