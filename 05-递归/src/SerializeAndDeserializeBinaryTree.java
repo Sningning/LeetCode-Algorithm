8ac546a890626d812c46691b3f5dee642b9430da
Song Ningning <qfsnn1117@gmail.com>
2020-05-03 17:20:08 +0800

    297 solved

--- 05-递归/src/SerializeAndDeserializeBinaryTree.java
+++ 05-递归/src/SerializeAndDeserializeBinaryTree.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- * TODO
- *
  * 297. 二叉树的序列化与反序列化
  * 序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，
  * 同时也可以通过网络传输到另一个计算机环境，采取相反方式重构得到原数据。
@@ -17,12 +19,50 @@ public class SerializeAndDeserializeBinaryTree {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        return null;
+        return serialize(root, "");
     }
+
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        return null;
+        String[] data_array = data.split(",");
+        List<String> data_list = new LinkedList<>(Arrays.asList(data_array));
+        return deserialize(data_list);
     }
 
+
+    private String serialize(TreeNode node, String str) {
+
+        if (node == null) return str += "null,";
+
+        str += node.val + ",";
+        str = serialize(node.left, str);
+        str = serialize(node.right, str);
+        return str;
+    }
+
+
+    private TreeNode deserialize(List<String> list) {
+
+        if (list.size() == 0) return null;
+        if (list.get(0).equals("null")) {
+            list.remove(0);
+            return null;
+        }
+
+        TreeNode node = new TreeNode(Integer.parseInt(list.get(0)));
+        list.remove(0);
+        node.left = deserialize(list);
+        node.right = deserialize(list);
+        return node;
+    }
+
+
+    public static void main(String[] args) {
+
+        SerializeAndDeserializeBinaryTree tree = new SerializeAndDeserializeBinaryTree();
+        TreeNode node = tree.deserialize("1,null,2");
+        String s = tree.serialize(node);
+        System.out.println(s);
+    }
 }
