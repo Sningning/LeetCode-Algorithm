e7880fad2c4d8dec3afa5c92d0daa6814392ae4a
Song Ningning <qfsnn1117@gmail.com>
2020-05-16 20:37:24 +0800

    update code

--- 08-二分查找/src/BinarySearch_704.java
+++ 08-二分查找/src/BinarySearch_704.java
@@ -11,6 +11,7 @@
 public class BinarySearch_704 {
 
     public int search(int[] nums, int target) {
+
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
@@ -23,5 +24,18 @@ public class BinarySearch_704 {
                 right = mid - 1;
         }
         return -1;
+
+
+        // int right = nums.length - 1;
+        // while (left < right) {
+        //     int mid = left + ((right - left) >>> 1);
+        //     if (nums[mid] == target)
+        //         return mid;
+        //     if (nums[mid] < target)
+        //         left = mid + 1;
+        //     else
+        //         right = mid - 1;
+        // }
+        // return nums[left] == target ? left : -1;
     }
 }
