045f27c5074c594aa9fc64b6e61a5183b947971e
Song Ningning <qfsnn1117@gmail.com>
2020-05-23 15:20:46 +0800

    update code

--- 08-二分查找/src/FindFirstAndLastPositionOfElementInSortedArray_34.java
+++ 08-二分查找/src/FindFirstAndLastPositionOfElementInSortedArray_34.java
@@ -14,68 +14,124 @@
  */
 public class FindFirstAndLastPositionOfElementInSortedArray_34 {
 
-    public int[] searchRange(int[] nums, int target) {
-        int len = nums.length;
-        if (len == 0 || target < nums[0] || target > nums[len - 1])
-            return new int[]{-1, -1};
-        int first = findFirstPosition(nums, target, len);
-        if (first == -1) {
-            return new int[]{-1, -1};
+     static class Solution1 {
+
+         public int[] searchRange(int[] nums, int target) {
+             int len = nums.length;
+             if (len == 0 || target < nums[0] || target > nums[len - 1])
+                 return new int[]{-1, -1};
+             int first = findFirstPosition(nums, target, len);
+             if (first == -1) {
+                 return new int[]{-1, -1};
+             }
+
+             int last = findLastPosition(nums, target, len);
+             return new int[]{first, last};
+         }
+
+
+         private int findFirstPosition(int[] nums, int target, int len) {
+             int left = 0;
+             int right = len - 1;
+             while (left < right) {
+                 int mid = left + ((right - left) >>> 1);
+                 if (nums[mid] < target) {
+                     // 如果 nums[mid] 严格小于 target，
+                     // 那么在 mid 位置以及 mid 之前的位置一定不存在目标元素
+                     // 下一轮搜索区间 [mid + 1, right]
+                     left = mid + 1;
+                 }
+                 else {
+                     // nums[mid] >= target
+                     // 下一轮搜索区间 [left, mid]
+                     // mid 存在的话一定存在于 [left, mid] 区间
+                     // 因为一直是 right 在收缩，所以找到第一个出现的位置
+                     right = mid;
+                 }
+             }
+             // target 可能不存在于数组中，需要进行判断
+             return nums[left] == target ? left : -1;
+         }
+
+
+         private int findLastPosition(int[] nums, int target, int len) {
+             int left = 0;
+             int right = len - 1;
+             while (left < right) {
+                 // 注意
+                 int mid = left + ((right - left + 1) >>> 1);
+                 if (nums[mid] > target) {
+                     // 如果 nums[mid] 严格大于 target，
+                     // 那么在 mid 位置以及 mid 之后的位置一定不存在目标元素
+                     // 下一轮搜索区间 [left, mid - 1]
+                     right = mid - 1;
+                 }
+                 else {
+                     // nums[mid] <= target
+                     // 下一轮搜索区间 [mid, right]
+                     // mid 存在的话一定存在于 [mid, right] 区间
+                     // 因为一直是 left 在收缩，所以找到最后出现的位置
+                     // 因为出现了 left = mid，所以应该取右中位数
+                     left = mid;
+                 }
+             }
+             // 主函数中先调用了 findFirstPosition，如果可以执行到该函数，
+             // 说明 target 在数组中一定存在，不需要再额外判断
+             return left;
+         }
+     }
+
+
+
+    static class Solution2 {
+        public int[] searchRange(int[] nums, int target) {
+            if (nums.length == 0)
+                return new int[]{-1,-1};
+            int first = findFirst(nums, target);
+            int last = findLast(nums, target);
+            return new int[]{first, last};
+
         }
 
-        int last = findLastPosition(nums, target, len);
-        return new int[]{first, last};
-    }
-
-
-    private int findFirstPosition(int[] nums, int target, int len) {
-        int left = 0;
-        int right = len - 1;
-        while (left < right) {
-            int mid = left + ((right - left) >>> 1);
-            if (nums[mid] < target) {
-                // 如果 nums[mid] 严格小于 target，
-                // 那么在 mid 位置以及 mid 之前的位置一定不存在目标元素
-                // 下一轮搜索区间 [mid + 1, right]
-                left = mid + 1;
+        private int findFirst(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left <= right) {
+                int mid = left + ((right - left) >>> 1);
+                if (nums[mid] == target)
+                    right = mid - 1;
+                else if (nums[mid] > target)
+                    right = mid - 1;
+                else if (nums[mid] < target)
+                    left = mid + 1;
             }
-            else {
-                // nums[mid] >= target
-                // 下一轮搜索区间 [left, mid]
-                // mid 存在的话一定存在于 [left, mid] 区间
-                // 因为一直是 right 在收缩，所以找到第一个出现的位置
-                right = mid;
-            }
+            return (left < nums.length && nums[left] == target ) ? left : -1;
         }
-        // target 可能不存在于数组中，需要进行判断
-        return nums[left] == target ? left : -1;
-    }
 
-
-    private int findLastPosition(int[] nums, int target, int len) {
-        int left = 0;
-        int right = len - 1;
-        while (left < right) {
-            // 注意
-            int mid = left + ((right - left + 1) >>> 1);
-            if (nums[mid] > target) {
-                // 如果 nums[mid] 严格大于 target，
-                // 那么在 mid 位置以及 mid 之后的位置一定不存在目标元素
-                // 下一轮搜索区间 [left, mid - 1]
-                right = mid - 1;
+        private int findLast(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left <= right) {
+                int mid = left + ((right - left) >>> 1);
+                if (nums[mid] == target)
+                    left = mid + 1;
+                else if (nums[mid] > target)
+                    right = mid - 1;
+                else if (nums[mid] < target)
+                    left = mid + 1;
             }
-            else {
-                // nums[mid] <= target
-                // 下一轮搜索区间 [mid, right]
-                // mid 存在的话一定存在于 [mid, right] 区间
-                // 因为一直是 left 在收缩，所以找到最后出现的位置
-                // 因为出现了 left = mid，所以应该取右中位数
-                left = mid;
-            }
+            return (right >= 0 && nums[right] == target) ? right : -1;
         }
-        // 主函数中先调用了 findFirstPosition，如果可以执行到该函数，
-        // 说明 target 在数组中一定存在，不需要再额外判断
-        return left;
+
+
+        public static void main(String[] args) {
+            int[] nums = {5,7,7,8,8,10};
+            Solution2 s = new Solution2();
+            System.out.println(s.findFirst(nums, 0));
+            System.out.println(s.findLast(nums, 12));
+        }
+
     }
+
 
 }
